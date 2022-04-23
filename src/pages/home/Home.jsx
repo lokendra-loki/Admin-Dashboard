af4d09efc65c9182b38e2bdc5339357948f18179
lokendra <lokendrachaulagain803@gmail.com>
2022-04-23 17:41:35 +0545

    user graph with api

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,7 @@
 import Chart from "../../components/chart/Chart";
 import FeaturedInfo from "../../components/featuredInfo/FeaturedInfo";
 import "./home.css";
-import { userData } from "../../dummyData";
+// import { userData } from "../../dummyData";
 import WidgetSm from "../../components/widgetSm/WidgetSm";
 import WidgetLg from "../../components/widgetLg/WidgetLg";
 import { useState, useEffect, useMemo } from "react";
@@ -9,7 +9,6 @@ import axios from "axios";
 
 
 export default function Home() {
-
   const MONTHS = useMemo(() => ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"], [])
 
   const [userStats, setUserStats] = useState([])
@@ -22,16 +21,17 @@ export default function Home() {
           }
         })
         // console.log(res)
-        res.data.map((item) => setUserStats(prev => [...prev, { name: MONTHS[item._id - 1], "New Users": item.total }]))
-
+        const statsList = res.data.sort(function (a, b) {
+          return a._id - b._id;
+        })
+        statsList.map((item) => setUserStats(prev => [...prev, { name: MONTHS[item._id - 1], "New User": item.total }]))
       } catch (error) {
         console.log(error)
       }
     }
     getStats()
   }, [MONTHS])
-
-  console.log(userStats)
+  // console.log(userStats)
 
 
 
@@ -41,10 +41,10 @@ export default function Home() {
   return (
     <div className="home">
       <FeaturedInfo />
-      <Chart data={userData} title="User Analytics" grid dataKey="Active User"/>
+      <Chart data={userStats} title="User Analytics" grid dataKey="New User" />
       <div className="homeWidgets">
-        <WidgetSm/>
-        <WidgetLg/>
+        <WidgetSm />
+        <WidgetLg />
       </div>
     </div>
   );
